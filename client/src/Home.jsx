fe651bdcaed15fdb0c93255c263653ac1102fc4a
Sharone Nixon <shanix@live.de>
2022-02-11 11:53:21 +0100

    adding to landingpage

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from "./images/CarGoSpace_logo.svg"
-import smLogo from "./images/CarGoSpace_logo_sm.svg"
+import "./Home.css"
 
 export default function Home() {
-    return <div className=''>
-        
-        <main className='d-flex flex-row align-items-center vh-100 '>
+    return <div className='home'>
+
+        <main className='d-flex flex-row align-items-center vh-100 mx-5 '>
             <div className='col-6'>
-                <h1>We love cars.</h1>
-                <h2>Social Network for <br /> Car enthusiasts.</h2>
-                <p>A place where you can connect with new people and share you stories, experience and love for cars</p>
+                <h1 id='title'>We love cars.</h1>
+                <h2 id='subtitle'>Social Network for <br /> Car enthusiasts.</h2>
+                <p id='subtext'>A place where you can connect with new people <br /> and share you stories, experience and <br /> love for cars.</p>
+                <button className='button'>Join the Family</button>
             </div>
             <img className='col-6 ' src={logo} alt="CarGoSpace Logo" />
         </main>
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -3,7 +3,7 @@ import smLogo from "./images/CarGoSpace_logo_sm.svg"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid ">
                 <a className="navbar-brand" href="#">
                     <img style={{height: "40px" }} src={smLogo} alt="small Logo" />
