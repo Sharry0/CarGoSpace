e49eb99dfc1afcbf1254c915e504cf54ecb59206
Sharone Nixon <shanix@live.de>
2022-02-24 12:22:06 +0100

    adding post section

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,6 +1,10 @@
 
-import React from 'react'
-import settingsIcon from "../images/icons/settings_icon.svg"
+import React from 'react';
+import settingsIcon from "../images/icons/settings_icon.svg";
+import emptyProfilImg from "../images/icons/empty_profil_img.svg";
+import commentIcon from "../images/icons/comment_icon.svg";
+import likeIcon from "../images/icons/like_icon.svg";
+import editIcon from "../images/icons/edit_icon.svg";
 
 
 export default function Feed() {
@@ -8,17 +12,23 @@ export default function Feed() {
         {
             postTitle: "Great car",
             postText: "i love this car, its very fast",
-            postAuthor: "Bobby"
+            postAuthor: "Bobby",
+            profilImg: "https://assets.cdn.moviepilot.de/files/6e4bc29be481dad6d2fd2b3011df1409083166f3e22a97bdae27a428dfff/fill/1200/576/Yoshi.jpg",
+            commentsNr: "35"
         },
         {
             postTitle: "Great car",
-            postText: "i love this car, its very fast",
-            postAuthor: "Bobby"
+            postText: " love this car.",
+            postAuthor: "Frank",
+            profilImg: "",
+            commentsNr: "25"
         },
         {
             postTitle: "Great car",
-            postText: "i love this car, its very fast",
-            postAuthor: "Bobby"
+            postText: " its very fast",
+            postAuthor: "John",
+            profilImg: "",
+            commentsNr: "5"
         }
     ]
 
@@ -29,9 +39,13 @@ export default function Feed() {
         objectFit: "cover"
     }
 
+    const postFooterStyling = {
+        height: "20px",
+        width: "20px"
+    }
+
     return (
         <div className='container pt-5'>
-
 
             <main className='d-flex justify-content-center'>
                 {/* _______________Sidebar content______________________________________ */}
@@ -74,12 +88,12 @@ export default function Feed() {
                 {/* _______________Create post & show feed content______________________________________ */}
                 <div className='col-6 mx-5'>
                     {/* _______________Create post______________________________________ */}
-                    <div className='card p-3 d-flex flex-row mb-3'>
+                    <div className='card p-3 d-flex flex-row mb-3 shadow'>
                         {/* _______________Profil picture icon______________________________________ */}
-                        <img src="https://assets.cdn.moviepilot.de/files/6e4bc29be481dad6d2fd2b3011df1409083166f3e22a97bdae27a428dfff/fill/1200/576/Yoshi.jpg" alt="" style={profileIconStyling} className="me-4" />
+                        <img src={emptyProfilImg} alt="" style={profileIconStyling} className="me-4" />
                         {/* _______________Create post input link______________________________________ */}
                         <input
-                            className="form-control"
+                            className="form-control bg-secondary bg-opacity-25"
                             type="text"
                             defaultValue="Create a post"
                             aria-label="readonly input example"
@@ -88,13 +102,27 @@ export default function Feed() {
                     </div>
                     {/* _______________Show feed content______________________________________ */}
                     <div className="card col-12 shadow my-3">
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">An item</li>
-                            <li className="list-group-item">A second item</li>
-                            <li className="list-group-item">A third item</li>
-                        </ul>
-                        <div className="card-footer">
-                            Card footer
+                        <div className="card-body">
+                            <div className='d-flex flex-row align-items-center '>
+                                <img src={emptyProfilImg} alt="" style={profileIconStyling} className="me-2" />
+                                <h6 className="card-subtitle text-muted">Some_Profilname</h6>
+                            </div>
+                            <h5 className="card-title fw-bold">Post title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div className="card-footer bg-secondary bg-opacity-25 d-flex flex-row ">
+                            <a href="/" role="button" className='text-decoration-none text-muted d-flex flex-row align-items-center me-3' >
+                                <img src={commentIcon} alt="" style={postFooterStyling} />
+                                <p className='my-0 ms-1'>{`45 Comments`}</p>
+                            </a>
+                            <a href="/" role="button" className='text-decoration-none text-muted d-flex flex-row align-items-center me-3' >
+                                <img src={likeIcon} alt="" style={postFooterStyling} />
+                                <p className='my-0 ms-1'>{`100 Likes`}</p>
+                            </a>
+                            <a href="/" role="button" className='text-decoration-none text-muted d-flex flex-row align-items-center me-3' >
+                                <img src={editIcon} alt="" style={postFooterStyling} />
+                                <p className='my-0 ms-1'>Edit</p>
+                            </a>
                         </div>
                     </div>
 
