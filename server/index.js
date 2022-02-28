74be669fc850bec2128ff6b2006762aaf56e6385
Sharone Nixon <shanix@live.de>
2022-02-28 12:56:07 +0100

    connected to mongoDB + setup .env

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config({ path: "./.env" })
+};
+
 const epxress = require("express");
 const app = epxress();
 
-app.get("/", (req, res)=>{
+const mongoose = require("mongoose")
+
+
+mongoose.connect(process.env.DB_URI)
+    .then(console.log("DB CONNECTED"))
+    .catch(err => console.log("CONNECTION ERROR DB", err))
+
+
+
+
+
+app.get("/", (req, res) => {
     res.send("welcome to port 8080")
 })
 
-app.listen(8080, (req, res)=>{
+app.listen(8080, (req, res) => {
     console.log("listening on port 8080")
 })
 
