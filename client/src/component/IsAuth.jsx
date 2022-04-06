8b71f9743e813ee68afa4c08bcb725271df6cd84
Sharone Nixon <shanix@live.de>
2022-04-06 10:06:57 +0200

    code cleanup

--- client/src/component/IsAuth.jsx
+++ client/src/component/IsAuth.jsx
@@ -1,16 +1,16 @@
 
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { CookieContext } from '../context/userContext';
 import Loading from '../pages/Loading';
 import { Navigate } from 'react-router-dom';
 
 export default function IsAuth({ children }) {
-    const cookie = useContext(CookieContext);
+    const {cookie, isLoading} = useContext(CookieContext);
 
     //_______ Add useLocation to redirect them back to original route they wanted to, after logging / signing in________
 
     //_______ if isLoading out of the cookieContext is false, then return child component or redirect to register page____________
-    if (!cookie.isLoading) return (cookie.cookie ? <>{children} </> : <Navigate to="/register" />);
+    if (!isLoading) return (cookie ? <>{children} </> : <Navigate to="/register" />);
 
     //_______ if isLoading is true, then render Loading page_________________
     return (<> <Loading /> </>);
